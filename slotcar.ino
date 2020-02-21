9f3475394cc19f118c2423785dfde54ce0fd77c3
Marek Nesvadba <briskcz@gmail.com>
2020-02-21 23:17:05 +0100

    changed to OOP and added some things

--- slotcar.ino
+++ slotcar.ino
@@ -1,106 +1,162 @@
 #include <SPI.h>
 #include <SD.h>
+#include <Arduino_LSM9DS1.h>
 
-
-// Motor driver pins
+// Motor driver
 #define STBY 6
 #define IN1 7
 #define IN2 8
 #define PWM 9
 
-// LED pins
+// LED
 #define LED_1 2
 #define LED_2 3
 #define LED_3 4
 #define LED_4 5
 
-// Hall sensor pins
+// Hall sensor
 #define HALL A0
+#define TRESHOLD_HIGH 500
+#define TRESHOLD_LOW 180
 
-// SD card pins
+#define TRACK_LENGTH 425
+
+// SD card
 #define CS 10
 
 // Variables
-boolean sd_init = false;
-File log_file;
-
-int hall_value;
-bool can_count = true;
-int rotations = 0;
-int track_length = 425;   // cm
-float tire_travel = 7.4;    // cm
-
 int state = 0;  // 0 - first slow lap, 1 = fast driving
 
+class SdCard{
+    private:
+       bool initialized = false;
+       File file;
+    
+    public:
+        void setup(){
+            if(SD.begin(CS)){
+                initialized = true;
+            }
+        }
 
-void drive(int speed){
-    digitalWrite(STBY, HIGH);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(PWM, speed);
-}
+        void write(String string){
+            if(initialized){
+                file = SD.open("log_file.txt", FILE_WRITE);
+                file.print(string);
+                file.close();                
+            }
+        }
+};
 
-void brake(){
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-    analogWrite(PWM,0);
-}
+class Motor {
+    private:
+        int current_speed;
 
-void motorOff(){
-    analogWrite(PWM,0);
-    digitalWrite(STBY, LOW);
-}
+    public:
+
+        void setup(){
+            pinMode(IN1, OUTPUT);
+            pinMode(IN2, OUTPUT);
+            pinMode(PWM, OUTPUT);
+            pinMode(STBY, OUTPUT);
+        }
+
+        void drive(int speed){
+            if(speed != current_speed){
+                digitalWrite(STBY, HIGH);
+                digitalWrite(IN1, HIGH);
+                digitalWrite(IN2, LOW);
+                analogWrite(PWM, speed);
+            }
+            current_speed = speed;
+        }
+
+        void brake(){
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, HIGH);
+            analogWrite(PWM,0);
+            current_speed = 0;
+        }
+
+        void turnOff(){
+            analogWrite(PWM,0);
+            digitalWrite(STBY, LOW);
+        }
+
+};
+
+class Hall {
+    private:
+        int value;
+        bool can_count = true;
+        int rotations = 0;
+        const float tire_circumference = 7.4;   // All lenghts are in cm
+        int traveled_distance = 0;
+    
+    public:
+    void loop(){
+        value = analogRead(HALL);
+
+        if(value > TRESHOLD_HIGH && can_count){
+            rotations ++;
+            traveled_distance = rotations * tire_circumference;
+            can_count = false;
+        }
+
+        if(value < TRESHOLD_LOW){
+            can_count = true;
+        }
+    }
+
+    int getDistance(){
+        return traveled_distance;
+    }
+
+};
+
+// TODO: class Led ....
+
+Motor motor;
+SdCard sd;
+Hall hall;
 
 void setup() {
-
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(PWM, OUTPUT);
-    pinMode(STBY, OUTPUT);
-
     pinMode(LED_1, OUTPUT);
     pinMode(LED_2, OUTPUT);
     pinMode(LED_3, OUTPUT);
     pinMode(LED_4, OUTPUT);
 
+    motor.setup();
+    sd.setup();
 
-    if(SD.begin(CS)){
-        sd_init = true;
+    sd.write("================================ NEW LOG ================================\n\n");
+
+    if (!IMU.begin()) {
+        sd.write("IMU inicialization failed!");
+        // TODO: do something with lights to signal failure
     }
-
-    if(sd_init){
-        log_file = SD.open("log_file.txt", FILE_WRITE);
-        log_file.println("================================ NEW LOG ================================\n");
-        log_file.close();
-    }
-
-    drive(45);
 
 }
 
 void loop() {
 
-    hall_value = analogRead(HALL);
+    hall.loop();
 
-    if(hall_value > 500 && can_count){
-        rotations ++;
-        if(sd_init){
-            log_file = SD.open("log_file.txt", FILE_WRITE);
-            log_file.print("Rotations: ");
-            log_file.println(rotations);
-            log_file.close();
+    if(hall.getDistance() > TRACK_LENGTH){
+        state = 1;
+    }
+
+    if(state == 0){
+        motor.drive(45);
+
+        float x,y,z;    // x - front and back, y - left and right, z - up and down
+        if (IMU.accelerationAvailable()) {
+            IMU.readAcceleration(x, y, z);
         }
-        can_count = false;
     }
 
-    if(hall_value < 180){
-        can_count = true;
-    }
-
-    float traveled_distance = tire_travel * rotations;
-
-    if(traveled_distance > track_length){
-        brake();
+    if(state == 1){
+        motor.brake();
     }
 
 }
