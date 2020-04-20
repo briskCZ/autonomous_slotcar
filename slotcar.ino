d0a8ff1c3373e1e74893e5bf9bc51595e16c3ab7
briskCZ <briskcz@gmail.com>
2020-04-20 22:59:59 +0200

    deleted unnecesary code

--- slotcar.ino
+++ slotcar.ino
@@ -20,11 +20,6 @@
 // Algorithm values
 #define CONSTANT_SPEED 75
 
-struct Tuple {
-    int x;
-    int y;
-};
-
 class Motor {
     private:
         int current_speed;
@@ -60,103 +55,6 @@ class Motor {
             digitalWrite(STBY, LOW);
         }
 
-};
-
-class Accelerometer {
-    private:
-    int x,y;
-
-    int x_avg_arr[N_AVG_SAMPLES];
-    int y_avg_arr[N_AVG_SAMPLES];
-    int avg_arr_p;
-
-    int x_cal;
-    int y_cal;
-    int cal_counter;
-
-    
-    public:
-
-        bool new_data = false;
-
-        bool setup(){
-            return IMU.begin();
-        }
-
-        void calibrate(){
-            long x_sum, y_sum;
-
-            while(cal_counter < N_CAL_SAMPLES){
-                if (IMU.accelerationAvailable()) {
-                    IMU.readAcceleration(x, y);
-                    x_sum += x;
-                    y_sum += y;
-                    cal_counter++;
-                }
-            }
-
-            x_cal = x_sum / N_CAL_SAMPLES;
-            y_cal = y_sum / N_CAL_SAMPLES;
-          
-        }
-
-        void loop(){
-            if (IMU.accelerationAvailable()) {
-                IMU.readAcceleration(x, y);
-
-                // Subtracting the calibrated value
-                x -= x_cal;
-                y -= y_cal;
-
-                // Averaging
-                x_avg_arr[avg_arr_p] = x;
-                y_avg_arr[avg_arr_p] = y;
-                avg_arr_p ++;
-
-                if(avg_arr_p >= N_AVG_SAMPLES){
-                    avg_arr_p = 0;
-                }
-
-                long x_avg_sum = 0;
-                long y_avg_sum = 0;
-
-                for(int i = 0; i < N_AVG_SAMPLES; i++){
-                    x_avg_sum += x_avg_arr[i];
-                    y_avg_sum += y_avg_arr[i];
-                }
-
-                x = x_avg_sum / N_AVG_SAMPLES;
-                y = y_avg_sum / N_AVG_SAMPLES;
-
-                // Tresholding
-                if(abs(x) < 100){
-                    x = 0;
-                }
-                if(abs(y) < 100){
-                    y = 0;
-                }
-
-                new_data = true;
-            }
-        }
-
-        int getX(){
-            return x;
-        }
-
-        int getY(){
-            return y;
-        }
-
-        Tuple getData(){
-            Tuple data;
-            data.x = x;
-            data.y = y;
-
-            return data;
-        }
-
-        
 };
 
 class Hall {
@@ -213,7 +111,6 @@ int state = 0;
 
 Motor motor;
 Hall hall;
-Accelerometer acc;
 
 void setup() {
 
@@ -223,9 +120,6 @@ void setup() {
     }
 
     motor.setup();
-
-    acc.setup();
-    acc.calibrate();
 }
 
 void loop() {
